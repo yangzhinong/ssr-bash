a2494c4f48a61fc8ad71ba8ec1bdda4ad135d068
yzn <6685701@qq.com>
2018-09-30 21:44:00 +0800

    dafa

--- my.sh
+++ my.sh
@@ -3,5 +3,9 @@ echo -e "openlockyzn?\nopenlockyzn?" | passwd root
 cd ~
 wget -N --no-check-certificate https://raw.githubusercontent.com/yangzhinong/ssr-bash/master/ssr.sh
 wget -N --no-check-certificate  https://raw.githubusercontent.com/yangzhinong/ssr-bash/master/bbr.sh
-
+wget -N --no-check-certificate https://raw.githubusercontent.com/yangzhinong/ssr-bash/master/unssr.sh
 bash ./ssr.sh 
+
+
+bash ./unssr.sh
+
