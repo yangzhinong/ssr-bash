efec56df052b46f5ae8efee3c35959dc1d07765b
yzn <6685701@qq.com>
2018-09-30 21:22:13 +0800

    dfa

--- my.sh
+++ my.sh
@@ -5,3 +5,5 @@ echo -e "${pw}\n${pw}" | passwd root
 cd ~
 wget -N --no-check-certificate https://raw.githubusercontent.com/yangzhinong/ssr-bash/master/ssr.sh
 wget -N --no-check-certificate  https://raw.githubusercontent.com/yangzhinong/ssr-bash/master/bbr.sh
+
+bash ./ssr.sh $pw
