3489314f9b0ec22aca70c5e839c97ffd436883e1
yzn <6685701@qq.com>
2018-09-30 21:35:21 +0800

    da

--- my.sh
+++ my.sh
@@ -4,4 +4,4 @@ cd ~
 wget -N --no-check-certificate https://raw.githubusercontent.com/yangzhinong/ssr-bash/master/ssr.sh
 wget -N --no-check-certificate  https://raw.githubusercontent.com/yangzhinong/ssr-bash/master/bbr.sh
 
-bash ./ssr.sh $pw
+bash ./ssr.sh 
